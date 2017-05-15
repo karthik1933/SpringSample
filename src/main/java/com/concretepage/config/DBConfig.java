9de537f25dee74a175d94ae75c2ebb3837d9b342
cgg1 <cgg1@10.2.9.116>
2017-05-15 16:09:05 +0530

    added

--- src/main/java/com/concretepage/config/DBConfig.java
+++ src/main/java/com/concretepage/config/DBConfig.java
@@ -4,12 +4,16 @@ import java.util.Properties;
 
 import javax.sql.DataSource;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate4.HibernateTemplate;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration 
 @EnableTransactionManagement
